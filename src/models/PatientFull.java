c17be499193e58275bf64668bec0276542f91b3f
CesarAcero97 <56569713+CesarAcero97@users.noreply.github.com>
2020-09-19 21:12:38 -0500

    Creación de clase PatientFull

---
+++ src/models/PatientFull.java
@@ -0,0 +1,27 @@
+package models;
+
+import java.time.LocalDate;
+
+public class PatientFull {
+	private int ID;
+	private LocalDate admissionDate;
+	private String code;
+	private String city;
+	private String department;
+	private String attention;
+	private byte age;
+	private String sex;
+	private String type;
+	private String status;
+	private String countryOfOrigin;
+	private LocalDate firstSymptomsDate;
+	private LocalDate dateOfDecease;
+	private LocalDate diagnosticDate;
+	private LocalDate recoveryDate;
+	private LocalDate webReportDate;
+	private String recoveryType;
+	private int departmentCode;
+	private int countryCode;
+	private String ethnicalPertenence;
+	private String ethnicalGroup;
+}
